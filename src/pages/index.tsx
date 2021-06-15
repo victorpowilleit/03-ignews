1948020cceb2dffe0b599108fe9acf573e4448f9
Victor Powilleit <victor.powilleit@gmail.com>
2021-06-15 08:31:49 -0300

    SSG Finished (1/3 ok) || Next: Back-end in Front-end (in Next)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import { GetServerSideProps } from "next";
+// import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import { SubscribeButton } from "../components/SubscribeButton";
 import styles from './home.module.scss'
 import { stripe } from "../services/stripe";
@@ -30,7 +31,8 @@ export default function Home({product}: HomeProps) {
   )
 }
 
-export const getServerSideProps:GetServerSideProps = async () => {
+// export const getServerSideProps:GetServerSideProps = async () => {
+export const getStaticProps:GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1Ix25JB4hIblJhBVpCmvJkLe', {expand: ['product']})
   const product = {
     priceId: price.id,
@@ -39,6 +41,7 @@ export const getServerSideProps:GetServerSideProps = async () => {
   return {
     props:{
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 }
